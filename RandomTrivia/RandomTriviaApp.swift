cab084b7b0e4820122aa09d0cd0614fa144d6251
Bradley Kang <bkang@lcs.on.ca>
2021-10-07 09:19:33 -0400

    Initial Commit

---
+++ RandomTrivia/RandomTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomTriviaApp.swift
+//  RandomTrivia
+//
+//  Created by Brad Kang on 2021-10-07.
+//
+
+import SwiftUI
+
+@main
+struct RandomTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
