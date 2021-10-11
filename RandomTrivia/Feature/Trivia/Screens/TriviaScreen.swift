e7a7b765f0404e0be88aa2442edf8ac2f86e32a3
Bradley Kang <bkang@lcs.on.ca>
2021-10-11 15:04:54 -0400

    Input dummy data for the answer buttons

--- RandomTrivia/Feature/Trivia/Screens/TriviaScreen.swift
+++ RandomTrivia/Feature/Trivia/Screens/TriviaScreen.swift
@@ -18,28 +18,28 @@ struct TriviaScreen: View {
             
             Spacer()
             
-            Button("Answer 1") {
+            Button(Trivia.dummyData.results[0].correct_answer) {
                 
             }
             .padding(3)
             .overlay(
             RoundedRectangle(cornerRadius: 16)
                 .stroke(Color.blue, lineWidth: 3))
-            Button("Answer 2") {
+            Button(Trivia.dummyData.results[0].incorrect_answers[0]) {
                 
             }
             .padding(3)
             .overlay(
             RoundedRectangle(cornerRadius: 16)
                 .stroke(Color.blue, lineWidth: 3))
-            Button("Answer 3") {
+            Button(Trivia.dummyData.results[0].incorrect_answers[1]) {
                 
             }
             .padding(3)
             .overlay(
             RoundedRectangle(cornerRadius: 16)
                 .stroke(Color.blue, lineWidth: 3))
-            Button("Answer 4") {
+            Button(Trivia.dummyData.results[0].incorrect_answers[2]) {
                 
             }
             .padding(3)
@@ -62,6 +62,13 @@ struct TriviaScreen: View {
         }
     }
 }
+
+// NEXT STEPS:
+// - Buttons when one is clicked, correct answer is shown and popup is given depending on if clicked answer is correct or not
+// - Change button amount and view when different amount of incorrect answers are given
+// - Randomise which button gives which answer
+// - Fix and optimize the UI
+// - Replayability.  I.e. when question is done, give another
 
 struct TriviaScreen_Previews: PreviewProvider {
     static var previews: some View {
