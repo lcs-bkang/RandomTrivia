86090bb2817c946ec0d4828798a6a7344dc06d63
Bradley Kang <bkang@lcs.on.ca>
2021-10-11 14:57:45 -0400

    Abstracted out the trivia questions and question type, etc.

--- RandomTrivia/Feature/Trivia/Screens/TriviaScreen.swift
+++ RandomTrivia/Feature/Trivia/Screens/TriviaScreen.swift
@@ -9,31 +9,12 @@ import SwiftUI
 
 struct TriviaScreen: View {
     
-    @StateObject private var vm = TriviaViewModelImpl(service: TriviaServiceImpl())
+    @StateObject var vm = TriviaViewModelImpl(service: TriviaServiceImpl())
     
     var body: some View {
         
         VStack {
-            Text(Trivia.dummyData.results[0].category)
-                .font(.title3)
-                .padding(3)
-                .overlay(
-                    Rectangle()
-                        .stroke(Color.blue, lineWidth: 4))
-            Text(Trivia.dummyData.results[0].question)
-                .font(.largeTitle)
-                .padding(5)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 16)
-                        .stroke(Color.blue, lineWidth: 6))
-            HStack {
-                Spacer()
-                Text("Difficulty: \(Trivia.dummyData.results[0].difficulty)")
-
-                Text("Type: \(Trivia.dummyData.results[0].type)")
-                Spacer()
-            }
-            .font(.headline)
+            TriviaView()
             
             Spacer()
             
--- RandomTrivia/Feature/Trivia/Views/TriviaView.swift
+++ RandomTrivia/Feature/Trivia/Views/TriviaView.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct TriviaView: View {
+    
+    @ObservedObject var vm = TriviaViewModelImpl(service: TriviaServiceImpl())
     var body: some View {
-        Text("Testing")
+        VStack {
+            Text(Trivia.dummyData.results[0].category)
+                .font(.title3)
+                .padding(3)
+                .overlay(
+                    Rectangle()
+                        .stroke(Color.blue, lineWidth: 4))
+            Text(Trivia.dummyData.results[0].question)
+                .font(.largeTitle)
+                .padding(5)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 16)
+                        .stroke(Color.blue, lineWidth: 6))
+            HStack {
+                Spacer()
+                Text("Difficulty: \(Trivia.dummyData.results[0].difficulty)")
+                
+                Text("Type: \(Trivia.dummyData.results[0].type)")
+                Spacer()
+            }
+            .font(.headline)
+        }
     }
 }
 
